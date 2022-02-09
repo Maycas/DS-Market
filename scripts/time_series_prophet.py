6af3304faec1dd24eb3aed581835db3c474d2622
Marc Maycas <marc.maycas@gmail.com>
2022-02-09 23:18:00 +0100

    Documented time_series_prophet.py functions

--- scripts/time_series_prophet.py
+++ scripts/time_series_prophet.py
@@ -5,6 +5,9 @@ from prophet.plot import plot_plotly, plot_components_plotly
 
 
 def clean_df(df_in):
+    """
+    Cleans and returns the 'df_in' dataframe to adapt it to prophet
+    """
     df = df_in.copy()
     df.index.name = 'ds'
     df.columns = ['store', 'item', 'y']
@@ -14,6 +17,10 @@ def clean_df(df_in):
 
 
 def filter_df(df, store=None, item=None):
+    """
+    Given a 'df' a 'store' identifier and an 'item' identifier it filters and prepares 
+    its data to be ingested into prophet as a dataframe with columns ds (date) and y
+    """
     if store and item:
         return df[(df.store == store) & (df.item == item)].drop(columns=['item', 'store'])
     elif store:
@@ -21,11 +28,17 @@ def filter_df(df, store=None, item=None):
     elif item:
         return df[df.item == item].drop(columns=['item', 'store']).groupby('ds').sum().reset_index()
     else:
-        print('A store or an item is mandatory')
+        print('A store and/or an item are mandatory')
         return None
 
 
 def train_prophet(df, predict_periods, holidays_list=None):
+    """
+    Trains and predicts a model given a prepared 'df' for a number of
+    'predict_periods' and also an optional 'holidays_list'.
+
+    Returns a trained 'model' and the prediction 'forecast'
+    """
     if holidays_list is None:
         model = Prophet()
     else:
@@ -39,6 +52,15 @@ def train_prophet(df, predict_periods, holidays_list=None):
 
 
 def plot_prophet_time_series(df, styles, model=None, forecast=None):
+    """
+    Given a 'df' containing the columns ds (dates) and y it plots a time series.
+
+    If a 'model' and a 'forecast' are given, it uses prophets plotting methods to
+    plot the predictions of the given 'model'.
+
+    It additionally requires a styles dictionary with title and label (xaxes_label
+    yaxes_label) information for the plotting
+    """
     if model is None:
         fig = px.line(
             df,
@@ -59,6 +81,14 @@ def plot_prophet_time_series(df, styles, model=None, forecast=None):
 
 
 def pipeline(df_in, predict_periods, store=None, item=None, holidays_list=None):
+    """
+    Executes the pipeline to create a model predictions for a 'df_in' dataframe,
+    for a given 'predict_periods'.
+
+    It requires either a 'store' code or an 'item' code to group data by.
+
+    Optionally, a 'holiday_list' can be given to feed the model with holiday dates information
+    """
     print('Cleaning Data...')
     df = clean_df(df_in)
 
